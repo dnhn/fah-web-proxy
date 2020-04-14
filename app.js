0180407a0dfa33dd9f5f43580f69c2d2e6e91abc
Nhan Doan <nhandev88@gmail.com>
2020-04-14 23:52:18 +0700

    Build proxy
    Run a proxy over Folding@home web client and capture response data
    on each work progress update request.

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const proxy = require('express-http-proxy');
+
+const HOST = 'http://127.0.0.1';
+const PORT = 4444;
+const FAH = `${HOST}:7396`;
+
+app.use(proxy(FAH, {
+  userResDecorator: (_, proxyData, userReq) => {
+    if (userReq.path === '/api/updates' && proxyData) {
+      console.log(JSON.parse(proxyData.toString('utf-8'))[0]);
+    }
+    return new Promise(resolve => resolve(proxyData));
+  }
+}));
+app.listen(PORT, () => console.log(`Proxy is running at ${HOST}:${PORT}\n`));
+
+module.exports = app;
